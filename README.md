a26e30f9e64bdfd56a6cb4ba6f39792579aea176
Solra Bizna <solra@bizna.name>
2017-09-24 15:49:44 -0600

    Better support for files, and other changes. - Instead of failing immediately if watching an argument fails, periodically retry. - If a watched file/folder is moved or deleted, try to watch whatever is created in its place. (This fixes the emacs backup bug.)

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ It only runs on Linux, because it uses inotify.
 
 Example usage:
 
-    overbuild path/to/oversub.sh src include
+    overbuild path/to/oversub.sh src include GNUmakefile
 
-This example will run `oversub.sh` every time any file in the `src` or `include` directory is modified. Any command or program can be used. If there are subdirectories of `src` or `include`, they will _not_ be monitored unless explicitly specified.
+This example will run `oversub.sh` every time any file in the `src` or `include` directory, or the file `GNUmakefile`, is modified. Any command or program can be used in place of `oversub.sh`. Files and directories may both be specified, but subdirectories of specified directories will _not_ be monitored unless they, themselves, are also explicitly specified. (e.g. the above example would not track changes in `src/teg`)
 
 The `oversub.sh` included with this package runs `make -O -k -j2` (-O = group job outputs together, -k = continue building as far as possible after error, -j2 = run two jobs in parallel) and digests the output. `oversub_j1.sh` is the same, but does a serial build rather than a parallel one.
 
-\* If you save a change, and then save a second change after that file is read during the build but before the output is saved, and the first build is successful, the second build may not detect the second change. I've been using overbuild for two years now and I've only had this happen twice. The situation can usually be recovered by simply saving the file again. Caveat user. I would fix this but the underlying circumstances are quite rare...
+\* If you save a change, and then save a second change after that file is read during the build but before the output is saved, and the first build is successful, the second build may not detect the second change. I've been using overbuild for two years now and I've only had this happen twice. The situation can usually be recovered by simply saving the file again. Caveat user. I would try to fix this but the underlying circumstances are quite rare...
--- overbuild.cc
+++ overbuild.cc
@@ -25,35 +25,102 @@
 #include <stdio.h>
 #include <errno.h>
 #include <limits.h>
+#include <sys/select.h>
 
-static int ifd; // Inotify File Descriptor
+#include <string>
+#include <list>
+#include <unordered_map>
 
-static void run_command(const char* path) {
-  system(path);
-}
-
-static void wait_for_events() {
-  static char buf[sizeof(struct inotify_event) + NAME_MAX + 1];
-  fcntl(ifd, F_SETFL, 0);
- again:
-  if(read(ifd, buf, sizeof(buf)) <= 0) {
-    switch(errno) {
-#if EAGAIN != EWOULDBLOCK
-    case EWOULDBLOCK:
-#endif
-    case EAGAIN:
-      return;
-    case EINTR: goto again;
-    default:
-      perror("read(ifd)");
-      exit(1);
-    }
+namespace {
+  int ifd; // Inotify File Descriptor
+  std::list<std::string> paths_to_watch;
+  std::unordered_map<int, std::string> watched_paths;
+  void run_command(const char* path) {
+    system(path);
   }
-  else {
-    auto p = (struct inotify_event*)buf;
-    if(p->len && (p->name[0] == '#' || p->name[p->len-1] == '~')) goto again;
-    fcntl(ifd, F_SETFL, O_NONBLOCK);
-    goto again;
+  void wait_for_events() {
+    static char buf[sizeof(struct inotify_event) + NAME_MAX + 1];
+    static bool initial = true;
+    fcntl(ifd, F_SETFL, 0);
+    while(true) {
+      if(!paths_to_watch.empty()) {
+        auto it = paths_to_watch.begin();
+        while(it != paths_to_watch.end()) {
+          auto dis = it++;
+          int wd = inotify_add_watch(ifd, dis->c_str(),
+                                     IN_CLOSE_WRITE|IN_IGNORED|IN_MOVE_SELF);
+          if(wd < 0) {
+            fprintf(stderr, "%s: %s\n",
+                    dis->c_str(), strerror(errno));
+          }
+          else {
+            if(!initial) {
+              fprintf(stderr, "%s: (now being watched)\n",
+                      dis->c_str());
+            }
+            watched_paths[wd] = *dis;
+            paths_to_watch.erase(dis);
+          }
+        }
+        initial = false;
+      }
+      bool ready_to_read;
+      if(!watched_paths.empty()) {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(ifd, &fds);
+        struct timeval timeout;
+        do {
+          // try calling select over and over again if necessary, until we end
+          // up with a non-error return
+          timeout = {2, 0};
+        } while(select(ifd+1, &fds, nullptr, nullptr, &timeout) < 0);
+        ready_to_read = FD_ISSET(ifd, &fds);
+      }
+      else ready_to_read = true;
+      if(ready_to_read) {
+        /* wait for at least one event */
+        if(read(ifd, buf, sizeof(buf)) <= 0) {
+          switch(errno) {
+#if EAGAIN != EWOULDBLOCK
+          case EWOULDBLOCK:
+#endif
+          case EAGAIN:
+            return; // this is where we exit the loop
+          case EINTR: continue;
+          default:
+            perror("read(ifd)");
+            exit(1);
+          }
+        }
+        else {
+          auto p = (struct inotify_event*)buf;
+          if(p->mask & IN_CLOSE_WRITE) {
+            if(p->len && (p->name[0] == '#' || p->name[p->len-1] == '~'))
+              continue;
+            /* enter non-blocking mode, so the loop becomes a poll */
+            fcntl(ifd, F_SETFL, O_NONBLOCK);
+            continue;
+          }
+          else if(p->mask & IN_MOVE_SELF) {
+            /* this will trigger IN_IGNORED, so we will re-watch whatever
+               appears in its place */
+            inotify_rm_watch(ifd, p->wd);
+          }
+          else if(p->mask & IN_IGNORED) {
+            auto it = watched_paths.find(p->wd);
+            if(it == watched_paths.end())
+              fprintf(stderr, "Warning: Got IN_IGNORED for unknown watch descriptor %i\n", p->wd);
+            else {
+              auto path = it->second;
+              fprintf(stderr, "%s was deleted or moved\n", path.c_str());
+              paths_to_watch.emplace_back(path);
+              watched_paths.erase(it);
+            }
+          }
+        }
+      }
+    }
   }
 }
 
@@ -65,10 +132,7 @@ int main(int argc, char* argv[]) {
   ifd = inotify_init();
   if(ifd < 0) { perror("inotify_init"); return 1; }
   for(int i = 2; i < argc; ++i) {
-    if(inotify_add_watch(ifd, argv[i], IN_CLOSE_WRITE) < 0) {
-      perror(argv[i]);
-      return 1;
-    }
+    paths_to_watch.emplace_back(argv[i]);
   }
   while(1) {
     run_command(argv[1]);
